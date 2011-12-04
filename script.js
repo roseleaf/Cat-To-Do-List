ee2cb384a70b1bea11110baba9e1e5681fb44583
roseleaf <pteroline@gmail.com>
2011-12-03 20:15:18 -0500

    converted to jquery

--- script.js
+++ script.js
@@ -1,71 +1,61 @@
-var todo_input = document.getElementById('to-do');
-var add_button = document.getElementById('add-to-do');
-var todo_space = document.getElementById('to-dos');
+$(document).ready(function() {
 
-todo_input.onkeypress = function(e){
-    if (e.keyCode == 13) {
-        addTodo();
-    }
-}
+    var todo_input = $('#to-do');
+    var add_button = $('#add-to-do');
+    var todo_space = $('#to-dos');
 
-add_button.onclick = addTodo;
-
-function addTodo() {
-    if(todo_input.value == ""){
-        alert("Enter a to-do item MEOW"); 
-        return;
-    }
-
-    var todo = document.createElement('div');
-    todo.className ="to-do clearfix";
-
-    var checkbox = document.createElement('div');
-    checkbox.className = "checkbox";
-    checkbox.onclick = function() {
-        if(todo.className == "to-do clearfix"){
-            todo.className = "to-do clearfix checked";
-        }else{
-            todo.className = "to-do clearfix";
+    todo_input.keypress(function(e){
+        if (e.keyCode == 13) {
+            addTodo();
         }
-    }
+    });
 
-    var title = document.createElement('div');
-    title.className = "title";
-    title.innerText = todo_input.value;
+    add_button.click(addTodo);
 
-    var remover = document.createElement('input');
-    remover.type = "button";
-    remover.className = "removerbutton";
-
-    var editline = document.createElement('input');
-    editline.type = "text";
-    editline.value = title.innerText; 
-    editline.className = "editline";
-    editline.style.display = "none";
-    editline.onkeypress = function(e){
-        if (e.keyCode == 13){
-            title.innerText = editline.value;
-            this.style.display = "none";
-            title.style.display = "";
+    function addTodo() {
+        if(todo_input.val() == ""){
+            alert("Enter a to-do item MEOW"); 
+            return;
         }
+
+        var todo = $('<div/>', {class: 'to-do clearfix'});
+
+        var checkbox = $('<div/>', {class: "checkbox"});
+        checkbox.click(function() {
+            todo.toggleClass("checked");
+        });
+
+        var title = $('<div/>', {class: "title", text: todo_input.val()});
+
+        var remover = $('<input/>', {type: "button", class: "removerbutton"});
+
+        var editline = $('<input/>', {type:"text", value: title.text(), class: "editline"});
+        editline.hide();
+
+        editline.keypress (function(e){
+            if (e.keyCode == 13){
+                title.text(editline.val());
+                $(this).hide();
+                title.show();
+            }
+        });
+
+        title.click (function(){
+            $(this).hide();
+            editline.show();
+        });
+
+        remover.click (function(){
+            if (confirm("Are you sure you want to delete this item?")){
+                todo.remove();  
+            }
+        })
+
+        todo.append(checkbox);
+        todo.append(title);
+        todo.append(editline);
+        todo.append(remover);
+        todo_space.append(todo);
+        todo_input.val("");
     }
-
-    title.onclick = function(){
-        this.style.display = "none";
-        editline.style.display = "";
-    }
-
-    remover.onclick = function(){
-        if (confirm("Are you sure you want to delete this item?")){
-            todo_space.removeChild(todo);   
-        }
-    }
-
-    todo.appendChild(checkbox);
-    todo.appendChild(title);
-    todo.appendChild(editline);
-    todo.appendChild(remover);
-    todo_space.appendChild(todo);
-    todo_input.value = "";
-}
-
+});
